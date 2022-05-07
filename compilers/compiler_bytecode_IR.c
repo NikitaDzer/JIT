f9795c0c6a9bdb30b29667d91fdcff9966e6ebfe
NikitaDzer <nikita.dzer@mail.ru>
2022-05-08 00:11:56 +0300

    [restructure]: compilers API were improved for project faster scaling

--- compilers/compiler_bytecode_IR.c
+++ compilers/compiler_bytecode_IR.c
@@ -90,11 +90,7 @@ typedef enum IntermediateStatus
 
 
 static const double APPROXIMATION_FACTOR = 1.2; // empirical coefficient
-static const unsigned char UNDEFINED_BYTECODE_INSTRUCTION_OPCODE = -1;
-static const unsigned char UNDEFINED_BYTECODE_INSTRUCTION_REGISTRY = -1;
-static const unsigned long long INCORRECT_BYTECODE_INSTRUCTION_ARGUMENT = -1ULL; // rename constant
-
-static const unsigned char N_PROCESSOR_REGISTERS = 16;
+static const unsigned char UNDEFINED_BYTECODE_OPCODE = -1;
 
 
 static inline list_index_t calc_IR_size_approximately(const size_t n_instructions)
@@ -129,14 +125,29 @@ static inline bool is_intermediate_jump_or_call(const unsigned char intermediate
     }
 }
 
-static inline unsigned char get_intermediate_registry(const double bytecode_instruction_registry)
+static inline IntermediateRegistry get_intermediate_registry(const double bytecode_registry)
 {
-    const unsigned char registry = (unsigned char)bytecode_instruction_registry;
+    switch ((BytecodeRegistry)bytecode_registry)
+    {
+        case BYTECODE_RAX: return RAX;
+        case BYTECODE_RBX: return RBX;
+        case BYTECODE_RCX: return RCX;
+        case BYTECODE_RDX: return RDX;
+        case BYTECODE_RDI: return RDI;
+        case BYTECODE_RSI: return RSI;
+        case BYTECODE_RBP: return RBP;
+        case BYTECODE_RSP: return RSP;
+        case BYTECODE_R8: return  R8;
+        case BYTECODE_R9: return  R9;
+        case BYTECODE_R10: return R10;
+        case BYTECODE_R11: return R11;
+        case BYTECODE_R12: return R12;
+        case BYTECODE_R13: return R13;
+        case BYTECODE_R14: return R14;
+        case BYTECODE_R15: return R15;
     
-    if (registry >= N_PROCESSOR_REGISTERS)
-        return UNDEFINED_BYTECODE_INSTRUCTION_REGISTRY;
-    
-    return registry;
+        default: return UNDEFINED_REGISTRY;
+    }
 }
 
 static inline unsigned char get_intermediate_opcode(const char bytecode_instruction_opcode)
@@ -149,6 +160,9 @@ static inline unsigned char get_intermediate_opcode(const char bytecode_instruct
         // POP
         case 2: return 0x02;
 
+        // OUT
+        case 6: return 0x05;
+        
         // JMP
         case 7: return 0x04;
         
@@ -158,7 +172,7 @@ static inline unsigned char get_intermediate_opcode(const char bytecode_instruct
         // RET
         case 12: return 0x00;
         
-        default: return UNDEFINED_BYTECODE_INSTRUCTION_OPCODE;
+        default: return UNDEFINED_BYTECODE_OPCODE;
     }
 }
 
@@ -216,11 +230,11 @@ static inline IntermediateStatus check_intermediate(const Intermediate *const re
 {
     // update log
     
-    if (intermediate->opcode == UNDEFINED_BYTECODE_INSTRUCTION_OPCODE)
+    if (intermediate->opcode == UNDEFINED_BYTECODE_OPCODE)
         return INTERMEDIATE_INCORRECT;
     
     if (intermediate->argument1.type == ARG_TYPE_REG || intermediate->argument1.type == ARG_TYPE_MEM_REG)
-        if (intermediate->argument1.registry == UNDEFINED_BYTECODE_INSTRUCTION_REGISTRY)
+        if (intermediate->argument1.registry == UNDEFINED_REGISTRY)
             return INTERMEDIATE_INCORRECT;
     
     if (is_intermediate_jump_or_call(intermediate->opcode))
