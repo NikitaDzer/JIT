177ca36742f81048cafa2d78ddc0ef52ed245e7d
NikitaDzer <nikita.dzer@mail.ru>
2022-05-14 21:27:25 +0300

    [buffer commit]

---
+++ include/optimizer.h
@@ -0,0 +1,12 @@
+// 
+// Created by MIPT student Nikita Dzer on 14.05.2022.
+// 
+
+#ifndef JIT_OPTIMIZER_H
+#define JIT_OPTIMIZER_H
+
+#include "IR.h"
+
+void optimize(IR *const restrict IR);
+
+#endif // JIT_OPTIMIZER_H
